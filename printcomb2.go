de8289839b9de6d31365957a6c62db759bb059c2
bekzhanovas <Abekzhanov@fortebank.com>
2019-10-16 21:22:02 +0600

    6

--- printcomb2.go
+++ printcomb2.go
@@ -5,8 +5,8 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			for k := '0'; k <= '9'; k++ {
-				for l := j; l <= '9'; l++ {
+			for k := i; k <= '9'; k++ {
+				for l := j + 1; l <= '9'; l++ {
 					z01.PrintRune(i)
 					z01.PrintRune(j)
 					z01.PrintRune(32)
@@ -23,3 +23,4 @@ func PrintComb2() {
 		}
 	}
 }
+
