c7e9891b24e9fe826339cd6aa2d2c4a6d2ac3127
bekzhanovas <Abekzhanov@fortebank.com>
2019-10-16 19:42:24 +0600

    dsfsd8d1122

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,10 +4,11 @@ import "fmt"
 
  
 func main() {
-    final := ""
-	for a := 97; a < 123; a++ {
-                character := rune(a)
-    final += string(character)}
-	fmt.Println(final)}
+		final := ""
+		for a := 97; a < 123; a++ {
+			character := rune(a)
+    	final += string(character)}
+		fmt.Println(final)
+	}
 
 				
