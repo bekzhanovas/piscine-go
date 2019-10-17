c24454518ebf82d0c2e5a8ffd1053f15722447da
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-17 14:29:37 +0600

    Update strrev.go

--- strrev.go
+++ strrev.go
@@ -2,7 +2,7 @@ package piscine
 
 func StrRev(s string) string {
 	last_string := ""
-	for i := 12 - 1; i >= 0; i-- {
+	for i := 50 - 1; i >= 0; i-- {
 		last_string += string(s[i])
 	}
 	return last_string
