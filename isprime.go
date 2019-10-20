299fc023cdd2db02743a8d84cf4035d9526434ef
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-20 15:19:01 +0600

    Create isprime.go

---
+++ isprime.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsPrime(nb int) bool {
+	counter := 0
+	for i := 1; i <= nb; i++ {
+		if nb%i == 0 {
+			counter++
+			if counter > 2 {
+				return false
+			}
+		}
+	}
+	return true
+}
