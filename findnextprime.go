eaa025f229a6e81d48367623cbc47f0676700e4c
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-21 19:00:38 +0600

    Update findnextprime.go

--- findnextprime.go
+++ findnextprime.go
@@ -1,21 +1,5 @@
 package piscine
 
-func IsPrime(nb int) bool {
-	if nb < 2 {
-		return false
-	}
-	counter := 0
-	for i := 1; i <= nb; i++ {
-		if nb%i == 0 {
-			counter++
-			if counter > 2 {
-				return false
-			}
-		}
-	}
-	return true
-}
-
 func FindNextPrime(nb int) int {
 	if nb < 2 {
 		return 2
