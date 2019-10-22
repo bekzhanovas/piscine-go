a949a44c60eb9a93521b535166ee2e7930d1f25f
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-22 11:09:41 +0600

    Update printnbrinorder.go

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,12 +3,28 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
+	var list []int
 	if n == 0 {
-		z01.PrintRune('0')
+		list = append(list, 0)
 	}
 	for n > 0 {
 		last := n % 10
 		n = n / 10
-		z01.PrintRune(rune(last) + '0')
+		list = append(list, last)
+	}
+	for {
+		counter := 0
+		for i := 1; i < len(list); i++ {
+			if list[i] < list[i-1] {
+				list[i], list[i-1] = list[i-1], list[i]
+				counter++
+			}
+		}
+		if counter == 0 {
+			break
+		}
+	}
+	for j := 0; j < len(list); j++ {
+		z01.PrintRune(rune(list[j]) + '0')
 	}
 }
