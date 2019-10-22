d98a07420e2450bfa885bda66ddb39a793447257
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-22 11:19:39 +0600

    Update printnbrinorder.go

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -4,6 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
 	var list []int
+	length := 0
 	if n == 0 {
 		list = append(list, 0)
 	}
@@ -11,10 +12,11 @@ func PrintNbrInOrder(n int) {
 		last := n % 10
 		n = n / 10
 		list = append(list, last)
+		length ++
 	}
 	for {
 		counter := 0
-		for i := 1; i < len(list); i++ {
+		for i := 1; i < length; i++ {
 			if list[i] < list[i-1] {
 				list[i], list[i-1] = list[i-1], list[i]
 				counter++
@@ -24,7 +26,7 @@ func PrintNbrInOrder(n int) {
 			break
 		}
 	}
-	for j := 0; j < len(list); j++ {
+	for j := 0; j < length; j++ {
 		z01.PrintRune(rune(list[j]) + '0')
 	}
 }
