f399177d3b80c90746d3b0dcbf76eb8ba9adb00a
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-22 18:55:19 +0600

    Create islower.go

---
+++ islower.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IsLower(str string) bool {
+	str1 := []rune(str)
+	for i := 0; i < StrLen2(str1); i++ {
+		if  (str1[i] >= 'a' && str1[i] <= 'z') {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
