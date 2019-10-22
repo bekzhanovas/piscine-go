3f3f4ff40564e46b5d731694ff9fde57a279bfa3
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-22 18:53:28 +0600

    Update isalpha.go

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(str string) bool {
 	str1 := []rune(str)
-	for i := 0; i < len(str1); i++ {
+	for i := 0; i < StrLen2(str1); i++ {
 		if (str1[i] >= '0' && str1[i] <= '9') || (str1[i] >= 'a' && str1[i] <= 'z') || (str1[i] >= 'A' && str1[i] <= 'Z') {
 			continue
 		} else {
