9551a3fccd1b1427a21fc843cd75e3a8d4325f73
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-22 18:45:08 +0600

    Create capitalize.go

---
+++ capitalize.go
@@ -0,0 +1,26 @@
+func Capitalize(s string) string {
+	s1 := []rune(s)
+	Is_capital := 1
+	for i := 0; i < len(s1); i++ {
+		if is_capital(s1[i]) == 1 && Is_capital == 1 {
+			if s1[i] >= 'a' && s1[i] <= 'z' {
+				s1[i] = s1[i] - 32
+			}
+			Is_capital = 0
+
+		} else if s1[i] >= 'A' && s1[i] <= 'Z' {
+			s1[i] = s1[i] + 32
+		} else if is_capital(s1[i]) == 0 {
+			Is_capital = 1
+		}
+	}
+	fmt.Println([]rune(s))
+	return string(s1)
+}
+
+func is_capital(a rune) int {
+	if (a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z') || (a >= '0' && a <= '9') {
+		return 1
+	}
+	return 0
+}
