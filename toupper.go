8f2e257d4fb500acdc2700bfc7f76a41dc1ed3cc
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-22 17:40:53 +0600

    Update toupper.go

--- toupper.go
+++ toupper.go
@@ -1,13 +1,13 @@
 package piscine
 
-func ToUpper(s string) string  {
+func ToUpper(s string) string {
 	s1 := []rune(s)
 	len_s := len(s1)
 	for i := 0; i < len_s; i++ {
-		if s1[i]>='a' && s1[i]<='z' {
-		s1[i]=s1[i]-32
+		if s1[i] >= 'a' && s1[i] <= 'z' {
+			s1[i] = s1[i] - 32
 		}
-		
+
 	}
 	return string(s1)
 }
