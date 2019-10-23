89e696325f23706b4f16c8f363f3d83d5c2850f8
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-23 10:00:41 +0600

    Update concat.go

--- concat.go
+++ concat.go
@@ -1,11 +1,6 @@
 package piscine
 
 func Concat(str1 string, str2 string) string {
-	str11 := []rune(str1)
-	str21 := []rune(str2)
-	str2_len := StrLen2(str21)
-	for i := 0; i < str2_len; i++ {
-		str11 = append(str11, str21[i])
-	}
-	return string(str11)
+	result:=str1+str2
+        return result
 }
