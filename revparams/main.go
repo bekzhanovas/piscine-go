0bcd5a39d1f5bfdbdeacfc5fab36f3e13eda40f5
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-23 15:21:58 +0600

    Update main.go

--- revparams/main.go
+++ revparams/main.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
 import "os"
+import "github.com/01-edu/z01"
 
 func main() {
-	data := os.args[1:]
-	for i := 0; i < StrLen2(data); i++ {
-		fmt.Println(i)
+	argument := os.Args
+	len_arg := 0
+	for k := range argument {
+		len_arg = k + 1
+	}
+
+	for i := len_arg - 1; i > 0; i-- {
+		a := []rune(argument[i])
+		for _, j := range a {
+			z01.PrintRune(j)
+		}
+		z01.PrintRune('\n')
 	}
 }
