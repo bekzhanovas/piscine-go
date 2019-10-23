bfebdf37c36ba0f0bc85c74db4b7a9e68202ac89
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-23 15:17:05 +0600

    Update main.go

--- printparams/main.go
+++ printparams/main.go
@@ -4,8 +4,11 @@ import "os"
 import "github.com/01-edu/z01"
 
 func main() {
-	argument := os.Args
-	len_arg := StrLen2(argument)
+	argument := []string{"askar", "bar", "dat"}
+	len_arg := 0
+	for k := range argument {
+		len_arg = k + 1
+	}
 	for i := 1; i < len_arg; i++ {
 		a := []rune(argument[i])
 		for _, j := range a {
