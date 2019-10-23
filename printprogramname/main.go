86d421a11987d3e967f04883cb7d2f82dfa4f000
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-23 10:15:29 +0600

    Update main.go

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func main() {
 	data := "./main"
 	data1 := []rune(data)
-	for i := 0; i < len(data1); i++ {
+	for i := 0; i < StrLen2(data1); i++ {
 		z01.PrintRune(data1[i])
 	}
 }
