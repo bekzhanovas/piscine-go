210b5f7d690a2430046c2762d63411c2b86ce620
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-24 12:11:23 +0600

    Update concatparams.go

--- concatparams.go
+++ concatparams.go
@@ -1,13 +1,14 @@
 package piscine
 
+
 func ConcatParams(args []string) string {
 	dl := 0
 	for i := range args {
-	dl = i + 1
+		dl = i + 1
 	}
 	ans := make([]string, dl)
 	for j := 0; j < (dl - 1); j++ {
-	ans[1] = ans[1] + args[j] + "\n"
+		ans[1] = ans[1] + args[j] + "\n"
 	}
 	ans[1] = ans[1] + args[(dl-1)]
 	return ans[1]
