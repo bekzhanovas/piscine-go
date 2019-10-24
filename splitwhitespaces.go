208d23ef6e3e8ab004c54f3f7eb9d14bb3c89154
bekzhanovas <53468401+bekzhanovas@users.noreply.github.com>
2019-10-24 15:26:16 +0600

    Update splitwhitespaces.go

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,19 +1,25 @@
 package piscine
 
 func SplitWhiteSpaces(str string) []string {
-	var data []string
 	str1 := []rune(str)
 	min := 0
 	max := 0
-	counter := 0
-	for _, letter := range str1 {
-		max++
-		counter++
-		if letter == ' ' || letter == '\n' || letter == '\t' {
-			data = append(data, str[min:max-1])
-			min = max
+	counter := 1
+	for _, i := range str1 {
+		if i == ' ' || i == '\n' || i == '\t' {
+			counter++
 		}
 	}
-	data = append(data, str[min:max])
+	data := make([]string, counter)
+	letter_number := 0
+	for _, letter := range str1 {
+		max++
+		if letter == ' ' || letter == '\n' || letter == '\t' {
+			data[letter_number] = str[min : max-1]
+			min = max
+			letter_number++
+		}
+	}
+	data[letter_number] = str[min:max]
 	return data
 }
